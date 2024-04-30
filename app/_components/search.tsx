f2327f3d8ed117f4aabf537970d333c64e9c6fed
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2024-04-30 14:56:50 -0300

    feat:initial add search

---
+++ app/_components/search.tsx
@@ -0,0 +1,17 @@
+// import { Icon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+  return (
+    <div className="flex gap-2">
+      <Input placeholder="Buscar Restaurantes" className="border-none" />
+      <Button size="icon">
+        <SearchIcon size={18} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Header from "./_components/header";
-
+import Search from "./_components/search";
 const Home = () => {
   return (
     // lembrar de quando chamar um elemento fazer </>
-    <Header />
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
   );
 };
 
