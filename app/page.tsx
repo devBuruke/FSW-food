256b8149b482d6dbf5e21bea0ee44ef9dac705ab
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2024-04-30 16:24:55 -0300

    feat:add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import Image from "next/image";
+
 const Home = () => {
   return (
     // lembrar de quando chamar um elemento fazer </>
@@ -11,6 +13,17 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/banner_01.png"
+          alt="até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
